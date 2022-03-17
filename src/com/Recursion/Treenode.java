418f71176ee8fc04a6a905f95d6d60a404c15323
Nitin <nsinghboss02@gmail.com>
2022-03-17 23:57:42 +0530

    the type of data explained in the paramaterised Constructor.

--- src/com/Recursion/Treenode.java
+++ src/com/Recursion/Treenode.java
@@ -2,11 +2,13 @@ package com.Recursion;
 import java.util.ArrayList;
 
 public class Treenode <T> {
-    // we have taken generics bcs the data can be of any type:
+    // we have taken generics bcz the data can be of any type:
     public T data;
     public ArrayList<Treenode<T>> children;
 
     // have taken arraylist in which the element will be treenode of type T.
+
+    // taken constuctor in which the data will be of T type.
 
     public Treenode(T data){
         this.data = data;
